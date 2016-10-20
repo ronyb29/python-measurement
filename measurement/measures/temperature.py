37ab4573adc6cb841348325e94b523cf61bfce3b
Rony Batista <rony@k3d.do>
2016-10-20 16:54:48 -0400

    Fixed bad import aliasing (whoops)

--- measurement/measures/temperature.py
+++ measurement/measures/temperature.py
@@ -1,12 +1,9 @@
-from decimal import Decimal
+from decimal import Decimal as D
 
 from measurement.base import MeasureBase, SimpleTransform as ST
 
-__all__ = [
-    'Temperature'
-]
+__all__ = ['Temperature']
 
-D = Decimal
 
 class Temperature(MeasureBase):
     STANDARD_UNIT = 'k'
@@ -15,7 +12,7 @@ class Temperature(MeasureBase):
                 fro=lambda k: k - D('273.15')),
         'f': ST(to=lambda f: (f + D('459.67')) * D(5) / D(9),
                 fro=lambda k: k * D(9) / D(5) - D('459.67')),
-        'k': Decimal('1.0')
+        'k': D('1.0')
     }
     ALIAS = {
         'celsius': 'c',
